2b24007daed88c0ce6858cc1717162ef696aa5e1
anoyos <anoyos.business@gmail.com>
2022-08-20 00:26:36 +0200

    vercel deploy test

--- src/App.js
+++ src/App.js
@@ -66,41 +66,6 @@ function App() {
         });
     };
 
-    const getData = () => {
-        if (blockchain.account !== "" && blockchain.smartContract !== null) {
-        dispatch(fetchData(blockchain.account));
-        }
-    };
-
-    const getConfig = async () => {
-        const configResponse = await fetch("/config/config.json", {
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        });
-        const config = await configResponse.json();
-        SET_CONFIG(config);
-    };
-
-    useEffect(() => {
-        getConfig();
-    }, []);
-
-    useEffect(() => {
-        getData();
-    }, [blockchain.account]);
-	
-	useEffect(() => {
-		const changeWidth = () => {
-		  setScreenWidth(window.innerWidth);
-		}
-		window.addEventListener('resize', changeWidth);
-		return () => {
-			window.removeEventListener('resize', changeWidth)
-		}
-	}, []);
-
 	const toggleNav = () => {
 		setToggleMenu(!toggleMenu)
 	}
